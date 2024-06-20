9438ccaa0249e1b5cb464fe713533dccf44ba629
Hiago Santos <157017143+HiagoBelar@users.noreply.github.com>
2024-06-20 20:03:57 -0300

    Update Conexao.java

--- src/br/com/formbelar/dao/Conexao.java
+++ src/br/com/formbelar/dao/Conexao.java
@@ -23,7 +23,7 @@ public class Conexao {
     
     public Connection getConnection () throws SQLException{
         
-        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/FormLoginAndRegister", "postgres", "SlipKnot123");
+        Connection conexao = DriverManager.getConnection("jdbc:postgresql://localhost:5432/FormLoginAndRegister", "postgres", "SenhaTeste123");
         return conexao;
     }
     
